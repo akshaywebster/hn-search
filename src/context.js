a58dfabb91ee20e8b0b2ba97f5c517c071b7272f
Akshay Chaudhary <akshaych49@gmail.com>
2022-03-17 00:39:00 +0530

    added fetchStoriesfunction & useEffect to display loading when app loads

--- src/context.js
+++ src/context.js
@@ -11,13 +11,29 @@ import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-const initialState = {}
+const initialState = {
+  isLoading: true, // state for loading
+}
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>
+
+  const fetchStories = async (url) => {
+    // const response = await fetch(url)
+    // const data = await response.json()
+    dispatch({ type: SET_LOADING })
+  }
+
+  useEffect(() => {
+    // when app loads, fetch the stories
+    fetchStories()
+  }, [])
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
